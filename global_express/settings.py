2cd34911a94f51ad9b1cf8ce31f595565586a715
Yardmanzee <yardmanzee0@protonmail.com>
2026-06-15 08:04:34 +0100

    debug == false

--- global_express/settings.py
+++ global_express/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 }
 
 STORAGES = {
-    'static': {
+    'staticfiles': {
         'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
     },
 }
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
