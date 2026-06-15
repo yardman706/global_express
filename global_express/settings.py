c263d27376cfa7f55dd60e55e20e093479ab9223
yardman706 <yardmanzee0@protonmail.com>
2026-06-15 08:32:59 +0100

    Update settings.py

--- global_express/settings.py
+++ global_express/settings.py
@@ -78,10 +78,9 @@ WSGI_APPLICATION = 'global_express.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 STORAGES = {
